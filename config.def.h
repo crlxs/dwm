776611a0959f9ab7a9fbd285818af3b85f48588a
crlxs <belmontecarles@gmail.com>
2024-05-22 10:15:34 +0200

    Swapped colors of selected/norm schemes

--- config.def.h
+++ config.def.h
@@ -19,8 +19,8 @@ static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_cyan, col_cyan },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_gray2  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
